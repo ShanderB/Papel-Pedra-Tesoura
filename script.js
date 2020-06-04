4b2f3d757abf1a385fce8409aefd98bf7d046e11
ShanderB <s.hander8910@hotmail.com>
2020-06-04 11:49:55 -0300

    Files Updates

--- script.js
+++ script.js
@@ -1,25 +1,37 @@
-
-
-
-
 /*
 
 1 = papel        
 2 = pedra
 3 = tesoura
 
-1*1/3=0,3333
+papel WIN pedra
+papel LOST tesoura
+
+pedra WiN tesoura
+pedra LOST papel
+
+tesoura WIN papel
+tesoura LOST pedra
+
+1*1/3=0,3333--
 1*2/3=0,6667
 1*3/3=1
 
-2*2/3=1,333
+2*2/3=1,333--
 2*3/3=2
 
-3*3/3=3
+3*3/3=3---
 */
 
 function jogar() {
     var movimento = document.getElementById("winner");
+    var player1 = document.getElementById("player1");
+    var player2 = document.getElementById("player2");
+
+    player1.src = "blank.jpg";
+    player2.src = "blank.jpg";
+    movimento.src = "blank.jpg";
+
     var game1 = Math.floor(Math.random() * 3) + 1;
     var game2 = Math.floor(Math.random() * 3) + 1;
     var winner = parseFloat((game1 * game2 / 3).toFixed(4));
@@ -29,17 +41,23 @@ function jogar() {
     switch (winner) {
         //Fazer aparecer os dois jogadores.
         //Adicionar counter
-        //
+        //Vai pegar qual sinal foi feito, se empatou, ganhou ou perdeu e checar o game1/2 conforme o valor final
         case 0.3333:
             console.log("Empate: \n0.333");
+            player1.src = "paper_draw.png";
+            player2.src = "paper_draw.png";
             break;
 
         case 3:
             console.log("Empate: \n3");
+            player1.src = "scissor_draw.png";
+            player2.src = "scissor_draw.png";
             break;
 
         case 1.3333:
             console.log("Empate: \n1.333");
+            player1.src = "rock_draw.png";
+            player2.src = "rock_draw.png";
             break;
 
         case 0.6667:
@@ -58,7 +76,7 @@ function jogar() {
             break;
 
         default:
-            console.log("Wait. What?" + a);
+            console.log("Wait. What?");
     }
 
 
