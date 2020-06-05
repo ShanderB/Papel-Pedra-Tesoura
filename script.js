ba91ff6c2047f9b80bf4e85258bcdf54a0fc2075
ShanderB <s.hander8910@hotmail.com>
2020-06-05 13:44:43 -0300

    Update Files

--- script.js
+++ script.js
@@ -15,30 +15,32 @@ tesoura LOST pedra
 
 1*1/3=0,3333--
 1*2/3=0,6667
-1*3/3=1
+1*3/3=1---
 
 2*2/3=1,333--
 2*3/3=2
 
 3*3/3=3---
 */
-
+let pointA = 0;
+let pointB = 0;
 function jogar() {
     var movimento = document.getElementById("winner");
     var player1 = document.getElementById("player1");
     var player2 = document.getElementById("player2");
     var point = document.getElementById("point");
+    var pointE = document.getElementById("pointE");
 
     player1.src = "blank.jpg";
     player2.src = "blank.jpg";
     movimento.src = "blank.jpg";
-    var pointA = 0;
+    
 
     var game1 = Math.floor(Math.random() * 3) + 1;
     var game2 = Math.floor(Math.random() * 3) + 1;
     var winner = parseFloat((game1 * game2 / 3).toFixed(4));
-/*  The first step will generate 2 random numbers;
-    The var "winner" will receive the calculus as an float fixed with 5 numbers;*/
+    /*  The first step will generate 2 random numbers;
+        The var "winner" will receive the calculus as an float fixed with 5 numbers;*/
 
     switch (winner) {
         //Fazer aparecer os dois jogadores.
@@ -65,26 +67,48 @@ function jogar() {
 
         case 0.6667:
             console.log("Papel: \n0.6667");
-            if(game1 == 1){
+            if (game1 == 1) {
                 pointA = pointA + 1;
                 player1.src = "paper.png";
                 player2.src = "rock.png";
                 point.innerHTML = pointA;
-            }else{
-                player2.src = "paper.png"; 
+            } else {
+                player2.src = "paper.png";
                 player1.src = "rock.png"
+                pointB = pointB + 1;
+                pointE.innerHTML = pointB;
             }
-            
+
             break;
 
         case 1:
             console.log("Tesoura: \n1");
-            game1 == 3 ? player1.src = "scissor.png" : player2.src = "scissor.png"
+            if (game1 == 1) {
+                pointA = pointA + 1;
+                player1.src = "scissor.png";
+                player2.src = "paper.png";
+                point.innerHTML = pointA;
+            } else {
+                player2.src = "scissor.png";
+                player1.src = "paper.png"
+                pointB = pointB + 1;
+                pointE.innerHTML = pointB;
+            }
             break;
 
         case 2:
             console.log("Pedra \n2");
-            game1 == 2 ? player1.src = "rock.png" : player2.src = "rock.png"
+            if (game1 == 2) {
+                pointA = pointA + 1;
+                player1.src = "rock.png";
+                player2.src = "scissor.png";
+                point.innerHTML = pointA;
+            } else {
+                player2.src = "rock.png";
+                player1.src = "scissor.png"
+                pointB = pointB + 1;
+                pointE.innerHTML = pointB;
+            }
             break;
 
         default:
